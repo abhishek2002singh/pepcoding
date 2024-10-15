7ed4ade88a2a5239223f945c280ee6537c5f3b19
abhishek singh <singhsabhishek23@gmail.com>
2024-10-15 16:33:19 +0530

    first commit

---
+++ introduction/TakeInput.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class TakeInput {
+    public static void main(String args[]) {
+        Scanner src = new Scanner(System.in);
+        int a = src.nextInt();
+        System.out.println("value is : " + a);
+    }
+}
---
+++ introduction/TakeStringInput.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class TakeStringInput {
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        String nameString = src.nextLine();
+        System.out.println("my name is : " + nameString);
+    }
+}
