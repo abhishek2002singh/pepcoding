6f6c79fe49767d036b7902fe25d0b7d3da6920dd
abhishek singh <singhsabhishek23@gmail.com>
2024-12-08 12:28:47 +0530

    print pattern question

--- pattern/tringle2.java
+++ pattern/tringle2.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
-public class tringel {
+public class tringle2 {
     public static void main(String[] args) {
         Scanner src = new Scanner(System.in);
         int n = src.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (j <= i) {
+                if (j >= 6 - i) {
                     System.out.print("*");
 
                 } else {
-                    System.out.print("");
+                    System.out.print(" ");
                 }
             }
             System.out.println();
